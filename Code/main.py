a3a958a55d1635d700e8bb4caa6cf3c4c5a55e37
Jan Kouřil <xkouril1@mendelu.cz>
2022-04-05 17:23:10 +0200

    Make heading with flask

--- Code/main.py
+++ Code/main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+from flask import Flask
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route("/")
+def heading_number_one():
+    return "<h1>Sea Rates Scraper!</h1>"
