372d39a5bc85bb2e65393b41ebbb9de1819ea9dd
AdamPech99 <104450200+AdamPech99@users.noreply.github.com>
2022-05-09 06:12:43 +0200

    remade init db

--- Code/init_db.py
+++ Code/init_db.py
@@ -1,16 +1,14 @@
 import sqlite3
 
 connection = sqlite3.connect('database.db')
-
-
-with open('schema.sql') as f:
-    connection.executescript(f.read())
-
 cur = connection.cursor()
-
-cur.execute("INSERT INTO countries VALUES(1,'Afghanistan','AF','AFG'),(2,'Aland Islands','AX','ALA'),(3,'Albania','AL','ALB'),(4,'Algeria','DZ','DZA'),(5,'American Samoa','AS','ASM'),(6,'Andorra','AD','AND'),(7,'Angola','AO','AGO'),(8,'Anguilla','AI','AIA'),(9,'Antarctica','AQ','ATA');")
-connection.commit()
-cur.execute("INSERT INTO ports VALUES(1, 'port1', 1)")
-cur.execute("INSERT INTO ports VALUES(2, 'port2', 1)")
+sql = "DROP TABLE IF EXISTS countries;"
+cur.execute(sql)
+sql = "DROP TABLE IF EXISTS ports;"
+cur.execute(sql)
+sql = "CREATE TABLE countries (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL);"
+cur.execute(sql)
+sql = "CREATE TABLE ports(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL,country_id INT,FOREIGN KEY(country_id) REFERENCES countries(id));"
+cur.execute(sql)
 connection.commit()
 connection.close()
