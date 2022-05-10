429bb4f69c8274972ce84f8ec040f414d00ef3a6
AdamPech99 <104450200+AdamPech99@users.noreply.github.com>
2022-05-10 05:27:20 +0200

    Added atributes
    Added atributes to port table

--- Code/init_db.py
+++ Code/init_db.py
@@ -6,9 +6,10 @@ sql = "DROP TABLE IF EXISTS countries;"
 cur.execute(sql)
 sql = "DROP TABLE IF EXISTS ports;"
 cur.execute(sql)
+connection.commit()
 sql = "CREATE TABLE countries (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL);"
 cur.execute(sql)
-sql = "CREATE TABLE ports(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL,country_id INT,FOREIGN KEY(country_id) REFERENCES countries(id));"
+sql = "CREATE TABLE ports(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL,country_id INT, address TEXT, port_auth TEXT, phone INT, fax INT, email TEXT, cords TEXT, cords_dec TEXT, un TEXT, type TEXT, size TEXT, website TEXT, terminal TEXT,FOREIGN KEY(country_id) REFERENCES countries(id));"
 cur.execute(sql)
 connection.commit()
 connection.close()
